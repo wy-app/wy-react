f5342da25560c44eca51fdd95a80818fe67ba0df
fuyz <953958160@qq.com>
2022-05-25 16:23:48 +0800

    关键字搜索联调

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -3,13 +3,15 @@ import type { ListInstance } from 'react-vant';
 import { getHotWord, getSearch } from '@/api'
 import { Cell, List, Empty, Search } from 'react-vant'
 import './index.scss'
-import { newsItem } from '../../types/news'
 import { /* BrowserRouter as Router, Switch, useParams, useLocation, */ useNavigate } from 'react-router-dom'
+import { LocationO } from '@react-vant/icons'
 
 export default function HotList () {
   const listRef = useRef<ListInstance>(null)
   const [dataList, setDataList] = useState([] as any)
+  const [searchList, setSearchList] = useState([] as any)
   const [finished, setFinished] = useState(false)
+  const [isSearch, setIsSearch] = useState(false)
   const [vauge, setVauge] = useState('');
   const navigate = useNavigate()
 
@@ -28,21 +30,36 @@ export default function HotList () {
       <Cell titleClass="search-title" valueClass="search-value" key={row.title} title={`${i + 1} ${row.hotWord}`} value={`${row.exp}`} isLink />
     </>
   }
-  const onChange = async () => {
+  const onSearch = async () => {
+    setIsSearch(true)
     const res: any = await getSearch({ callback: 'callback_fyz' }, { query: vauge })
-    console.log(res)
+    if (!res) return
+    const matchArr = res.match(/callback_fyz\((.*)\)/)
+    const resultStr = JSON.parse(matchArr[1])
+    const { data } = JSON.parse(resultStr)
+    console.log(data)
+    setSearchList(data.text)
   }
 
   return (
     <>
-      <Search className='search' shape="round" background="#fff" value={vauge} onChange={onChange} placeholder="请输入搜索关键词" showAction onCancel={() => navigate('/')} />
-      <List className='search-list' ref={listRef} finished={finished} errorText="请求失败，点击重新加载" onLoad={onLoad}>
-        {dataList.length
-          ? dataList.map((row: any, i: number) => {
-            return renderRow(row, i)
-          })
-          : <Empty description="暂无数据" />}
-      </List>
+      <Search className='search' shape="round" background="#fff" value={vauge} onChange={setVauge} onSearch={onSearch} placeholder="请输入搜索关键词" showAction onCancel={() => navigate('/')} />
+      {!isSearch
+        ? <List className='search-list' ref={listRef} finished={finished} errorText="请求失败，点击重新加载" onLoad={onLoad}>
+          {dataList.length
+            ? dataList.map((row: any, i: number) => {
+              return renderRow(row, i)
+            })
+            : <Empty description="暂无数据" />}
+        </List>
+        : <>{
+          searchList.length
+            ? searchList.map((e: any, i: number) => {
+              return <Cell className='search-title' title={e.suggestion} icon={<LocationO />} />
+            })
+            : <Empty description="暂无数据" />
+        }</>
+      }
     </>
   )
 }
