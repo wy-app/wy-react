af011ae3779abf8dcd18e078570089ab8d30c484
fuyz <953958160@qq.com>
2022-06-25 18:30:42 +0800

    评论联调完善

--- src/pages/newsDetail/Detail.tsx
+++ src/pages/newsDetail/Detail.tsx
@@ -24,7 +24,6 @@ const shareOptions = [
 function Detail (props: any) {
   const localtion = useLocation()
   const { id, activeTab, from } = localtion.state as any
-  debugger
   if (!id) return <Empty description="暂无数据" />
   const [detailData, setDetailData] = useState({} as DetailDataType)
   const navigate = useNavigate()
